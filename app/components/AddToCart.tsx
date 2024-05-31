19bc814f8bf5975e150942f1bb3fb347a6d87870
Nicolas <cyberniko@gmail.com>
2024-05-31 08:23:30 +0200

    31 mai

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const AddToCart = () => {
+    return (
+      <div>
+        <button onClick={() => console.log('Clickkkk')}>add to cart</button>
+    </div>
+  )
+}
+
+export default AddToCart
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+      <div>
+          <p>ProductCard</p>
+            <AddToCart/>
+      </div>
+
+  )
+}
+
+export default ProductCard
